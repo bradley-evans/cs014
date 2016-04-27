4fcd043e41d25d2ff8bc43dc62fb0db488026f9a
Bradley Evans <bevan006@ucr.edu>
2016-04-27 19:21:01 +0000

    The removal algorithm now seems to function.

--- PROGRAM2/BSTree.cpp
+++ PROGRAM2/BSTree.cpp
@@ -115,49 +115,73 @@ void BinarySearchTree::remove_search(Node* node, const string &string) {
     }
 }
 void BinarySearchTree::remove_node(Node* node) { // actually delete a node
-    // there are five cases.
-    Node *swapNode;
-    //case 1: a leaf
+    // there are five cases. case 0 is an empty tree, which is handled above.
+    //Node *swapNode;
+    
+    cout << "REMOVE OPERATION: DELETING [" << node->data << "]...";
+    //case 1: a leaf, simply delete it
     if (node->left == 0 && node->right == 0) {
-        if (node->data < node->up->data) {
+        cout << "Leaf...";
+        if (node->data > node->up->data) {
+            cout << "Right Child...";
             // then this is the right child and a leaf
             node->up->right = 0;
             delete node;
+            cout << "Deleted node." << endl;
         } else {
             // then it must be the left child
+            cout << "Left Child...";
             node->up->left = 0;
             delete node;
+            cout << "Deleted node." << endl;
         }
     
     // case 2: a node with one left child
     } else if (node->left != 0 && node->right == 0) {
-        cout << "Node with one left child." << endl;
-        swapNode = maxNode(node->left);
-        swapNode->up->right = 0;
-        node->data = swapNode->data;
-        node->count = swapNode->count;
-        delete swapNode;
+        cout << "Node with one left child...";
+        if (node->data > node->up->data) {
+            // then node is the right child of parent
+            cout << "Right child of parent...";
+            node->up->right = node->left;
+            delete node;
+            cout << "Deleted node." << endl;
+        } else {
+            // then it must be the left child of parent
+            cout << "Left child of parent...";
+            node->up->left = node->left;
+            delete node;    
+            cout << "Deleted node." << endl;
+        }
     
     // case 3: a node with one right child
     } else if (node->left == 0 && node->right != 0) {
-        cout << "Node with one right child." << endl;  
-        swapNode = minNode(node->right);
-        node->data = swapNode->data;
-        node->count = swapNode->count;
-        cout << "The minimum node was " << node->data << " (" << node->count << ")" << endl;
+        cout << "Node with one right child...";  
+        if (node->data < node->up->data) {
+            cout << "Left child of parent...";
+            // then node is the right child of parent
+            node->up->left = node->right;
+            delete node;
+            cout << "Deleted node." << endl;
+        } else {
+            cout << "Right child of parent...";
+            // then it must be the left child of parent
+            node->up->right = node->right;
+            delete node;       
+            cout << "Deleted node." << endl;
+        }
         
-        swapNode->up->left = 0;
-        delete swapNode;
-    
     // case 4: a node with two children
     } else if (node->left != 0 && node->right != 0) {
-        cout << "Node with two children." << endl;
-        // treat it as a node with only a right child
-        swapNode = minNode(node->right);
-        swapNode->up->left = 0;
-        node->data = swapNode->data;
-        node->count = swapNode->count;
-        delete swapNode;
+        cout << "Node with two children...";
+        // copy all data from right child into this node
+        // recursively call remove on right child
+        string tempdata = node->right->data;
+        int tempcount = node->right->count;
+        cout << "This node's data will be changed to [" << tempdata << "]. Calling remove recursively on " << node->right->data << endl;
+        remove_node(node->right);
+        node->data = tempdata;
+        node->count = tempcount;
+        cout << "Two-child node deletion complete." << endl;;
     }
 }
 Node* BinarySearchTree::minNode(Node* node) {
--- PROGRAM2/main.cpp
+++ PROGRAM2/main.cpp
@@ -9,7 +9,7 @@ int main() {
     string currstring;
     int i;
     
-    for (i=0;i<15;i++) {
+    for (i=0;i<20;i++) {
         cin >> currstring;
         tree.insert(currstring);
         cout << "Added " << currstring << " to tree." << endl;
@@ -27,8 +27,34 @@ int main() {
     cout << tree.largest() << endl;
     cout << "Smallest item in tree: ";
     cout << tree.smallest() << endl;
+    cout << "Deleting 'quebec' ..." << endl;
+    tree.remove("quebec");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
+    cout << "Deleting 'charlie' ..." << endl;
+    tree.remove("charlie");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
     cout << "Deleting 'juliet' ..." << endl;
     tree.remove("juliet");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
+    cout << "Deleting 'epsilon' ..." << endl;
+    tree.remove("epsilon");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
+    cout << "Deleting 'golf' ..." << endl;
+    tree.remove("golf");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
+    /*
+    cout << "Deleting 'alpha' ..." << endl;
+    tree.remove("alpha");
     cout << "In Order: ";
     tree.inOrder();
     cout << endl;
@@ -42,6 +68,22 @@ int main() {
     cout << tree.largest() << endl;
     cout << "Smallest item in tree: ";
     cout << tree.smallest() << endl;
+    cout << "Deleting 'quebec' ..." << endl;
+    tree.remove("quebec");
+    cout << "In Order: ";
+    tree.inOrder();
+    cout << endl;
+    cout << "Pre Order: ";
+    tree.preOrder();
+    cout << endl;
+    cout << "Post Order: ";
+    tree.postOrder();
+    cout << endl;
+    cout << "Largest item in tree: ";
+    cout << tree.largest() << endl;
+    cout << "Smallest item in tree: ";
+    cout << tree.smallest() << endl;
+    */
     
     
 }
